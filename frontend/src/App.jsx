89066204253d1ab2a8fca1b7b28b9809b40e157a
Connor Pietrasik <connorgpietrasik@gmail.com>
2026-06-22 04:32:04 -0700

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>Kindness is Magic</h1>
+      <p>Welcome to the React frontend! This is a development build.</p>
+    </div>
+  );
+}
+
+export default App;
